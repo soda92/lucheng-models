4ce7466e56001668357dce82b8d9e43ea5f5f4e6
Maple <wjxa20152015@gmail.com>
2025-07-25 15:40:55 +0800

    简化引用

---
+++ lucheng_models/__init__.py
@@ -0,0 +1,9 @@
+from lucheng_models.contract_record import save_or_update_contract_record
+from lucheng_models.health_record import save_or_update_health_record
+from lucheng_models.patient_registration import save_or_update_patient_registration
+
+__all__ = [
+    'save_or_update_contract_record',
+    'save_or_update_health_record',
+    'save_or_update_patient_registration',
+]
